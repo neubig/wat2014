9854b8ff095c8d75cd1119b1b62f4383a9969082
alvations <alvations@gmail.com>
2015-07-19 22:08:20 +0200

    Use dynamic path for travatar dir (TDIR) and util-script (UDIR)

--- script/train-lm.sh
+++ script/train-lm.sh
@@ -9,14 +9,15 @@ TDIR=$WD/tools/travatar
 NDIR=$WD/tools/nile
 MDIR=$MD/tools/mosesdecoder
 SDIR=$MD/tools/srilm
+UDIR=$WD/tools/util-scripts
 CORES=`nproc`
 
 # ********* LM preprocessing *********
 if [[ ! -e lm/data ]]; then
     mkdir -p lm/data
-    cat lm/raw/*.ja | sed 's/、/，/g;  s/（）//g; s/ //g' | ~/work/util-scripts/han2zen.pl --nospace | $KDIR/src/bin/kytea -model $KDIR/data/model.bin -notags -wsconst D > lm/data/all.ja
-    cat lm/raw/*.en | $TDIR/src/bin/tokenizer | sed "s/[     ]+/ /g; s/^ +//g; s/ +$//g" | ~/work/travatar/src/bin/tree-converter -input_format word -output_format word -split '(-|\\/)' | $TDIR/script/recaser/truecase.pl --model ja-en/preproc/train/truecaser/en.truecaser > lm/data/all.en
-    cat lm/raw/*.zh | $KDIR/src/bin/kytea -model $KDIR/data/ctb-0.4.0-5.mod | sed 's/（ *） *//g' | ~/work/util-scripts/han2zen.pl --nospace 2> /dev/null > lm/data/all.zh
+    cat lm/raw/*.ja | sed 's/、/，/g;  s/（）//g; s/ //g' | $UDIR/han2zen.pl --nospace | $KDIR/src/bin/kytea -model $KDIR/data/model.bin -notags -wsconst D > lm/data/all.ja
+    cat lm/raw/*.en | $TDIR/src/bin/tokenizer | sed "s/[     ]+/ /g; s/^ +//g; s/ +$//g" | $TDIR/src/bin/tree-converter -input_format word -output_format word -split '(-|\\/)' | $TDIR/script/recaser/truecase.pl --model ja-en/preproc/train/truecaser/en.truecaser > lm/data/all.en
+    cat lm/raw/*.zh | $KDIR/src/bin/kytea -model $KDIR/data/ctb-0.4.0-5.mod | sed 's/（ *） *//g' | $UDIR/han2zen.pl --nospace 2> /dev/null > lm/data/all.zh
 fi
 
 # ********* LM training *********
