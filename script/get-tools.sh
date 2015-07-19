625b8f03b77c23c7d312817b0e7d5a114cef2276
alvations <alvations@gmail.com>
2015-07-19 22:08:54 +0200

    Update get-tools.sh

--- script/get-tools.sh
+++ script/get-tools.sh
@@ -100,7 +100,7 @@ fi
 
 # Install util-scirpts in ~/work/
 if [[ ! -e $WD/work ]]; then
-    git clone https://github.com/neubig/util-scripts.git $WD/work
+    git clone https://github.com/neubig/util-scripts.git $TD/util-scripts
 fi
     
 
